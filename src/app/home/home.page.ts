aecf7a29a1976335510434b2c5e50cc2bca70586
Vincenzo D'Amico <vincenzodamico93@gmail.com>
2022-05-30 10:22:26 +0200

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component } from '@angular/core';
+import { SpeechRecognition } from '@capacitor-community/speech-recognition';
+import { TextToSpeech } from '@capacitor-community/text-to-speech';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +8,50 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  recording = false;
+  myText = 'Ciao';
 
-  constructor() {}
+  constructor(private changeDetectorRef: ChangeDetectorRef) {
+    SpeechRecognition.requestPermission();
+  }
+
+  async startRecognition() {
+    const { available } = await SpeechRecognition.available();
+
+    if (available) {
+      this.recording = true;
+      SpeechRecognition.start({
+        popup: true,
+        partialResults: true,
+        language: 'it-IT'
+        // maxResult
+      });
+
+      SpeechRecognition.addListener('partialResults', (data: any) => {
+        if (data.matches && data.matches.length > 0) {
+          this.myText = data.matches[0];
+          this.changeDetectorRef.detectChanges();
+        }
+
+        // Android has different result type
+        if (data.value && data.value.length > 0) {
+          this.myText = data.value[0];
+          this.changeDetectorRef.detectChanges();
+        }
+      });
+    }
+  }
+
+  async stopRecognition() {
+    this.recording = false;
+    await SpeechRecognition.stop();
+  }
+
+  speakText() {
+    TextToSpeech.speak({
+      text: this.myText,
+      lang: 'it-IT'
+    })
+  }
 
 }
